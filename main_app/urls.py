316fd1eecb93773f97de671a83e01b4d4ef648e8
rsato1007 <robertmsato@gmail.com>
2021-10-07 10:43:06 -0600

    setting up database and URLs

---
+++ main_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+"""
+This is be all our URLs, let's start formulating what URLs I will need.
+"""
+
+# this like app.use() in express
+urlpatterns = [
+
+]
